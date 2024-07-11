e7259ceb81d0ae68dfe2e005dae91cdd09c7550f
ryleighrowell <ryleighjean15@gmail.com>
2024-07-11 15:18:47 -0700

    scrip memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,27 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string reference = "Phillipians 4:13";
-        string scriptureText = "I can do all things through Christ which strengtheneth me.";
+        Reference reference = new Reference("Philippians 4:13");
+        Scripture scripture = new Scripture(reference, "I can do all things through Christ which strengtheneth me.");
 
-        List<string> words = scriptureText.Split(' ').ToList();
         Random random = new Random();
 
         while (true)
         {
             Console.Clear();
-            Console.WriteLine($"{reference}\n");
-
-            foreach (string word in words)
-            {
-                Console.Write(word + " ");
-            }
+            Console.WriteLine(scripture.GetDisplayText());
 
             Console.WriteLine("\n\nPress Enter to hide words or type 'quit' to exit.");
             string input = Console.ReadLine();
@@ -31,37 +24,86 @@ class Program
                 break;
             }
 
-            HideRandomWords(words, random);
+            scripture.HideRandomWords(random);
 
-            if (words.All(word => word == "_____"))
+            if (scripture.AllWordsHidden())
             {
                 Console.Clear();
-                Console.WriteLine($"{reference}\n");
-
-                foreach (string word in words)
-                {
-                    Console.Write(word + " ");
-                }
+                Console.WriteLine(scripture.GetDisplayText());
 
                 Console.WriteLine("\n\nAll words are hidden. The program will now exit.");
                 break;
             }
         }
     }
+}
 
-    static void HideRandomWords(List<string> words, Random random)
+class Reference
+{
+    public string Text { get; }
+
+    public Reference(string text)
     {
-        int wordsToHide = random.Next(1, Math.Max(1, words.Count / 10));
+        Text = text;
+    }
+}
+
+class Word
+{
+    public string Text { get; private set; }
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
+    }
+
+    public void Hide()
+    {
+        IsHidden = true;
+    }
+
+    public override string ToString()
+    {
+        return IsHidden ? "_____" : Text;
+    }
+}
+
+class Scripture
+{
+    public Reference Reference { get; }
+    private List<Word> Words { get; }
+
+    public Scripture(Reference reference, string text)
+    {
+        Reference = reference;
+        Words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{Reference.Text}\n\n{string.Join(" ", Words)}";
+    }
+
+    public void HideRandomWords(Random random)
+    {
+        int wordsToHide = random.Next(1, Math.Max(1, Words.Count / 10));
 
         for (int i = 0; i < wordsToHide; i++)
         {
             int index;
             do
             {
-                index = random.Next(words.Count);
-            } while (words[index] == "_____");
+                index = random.Next(Words.Count);
+            } while (Words[index].IsHidden);
 
-            words[index] = "_____";
+            Words[index].Hide();
         }
+    }
+
+    public bool AllWordsHidden()
+    {
+        return Words.All(word => word.IsHidden);
     }
 }
